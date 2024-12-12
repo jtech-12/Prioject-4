288aafb1c65eb9c83289baa8d02db2199bd0a44e
jtech-12 <brezzy868@gmail.com>
2024-12-12 14:36:16 -0500

    Update uiRoutes.js

--- routes/uiRoutes.js
+++ routes/uiRoutes.js
@@ -3,11 +3,11 @@ const router = express.Router();
 const path = require('path');
 
 router.get('/registrations/register', (req, res) => {
-  res.sendFile(path.join(__dirname, '../resources/html/register.html'));
+  res.sendFile(path.join(__dirname, '../resources/views/register.html'));
 });
 
 router.get('/registrations/view', (req, res) => {
-  res.sendFile(path.join(__dirname, '../resources/html/view.html'));
+  res.sendFile(path.join(__dirname, '../resources/views/view.html'));
 });
 
 module.exports = router;
